fb269c3afc5185035b9dbb31f176f33088cfd081
andreazanola98 <andrea.zanola98@gmail.com>
2023-10-10 13:08:09 +0200

    .

--- __lib/preprocess_all.m
+++ __lib/preprocess_all.m
@@ -16,6 +16,9 @@ clc
 
 modality = 'local'; % or local
 use_parpool = true; % use parpool if available
+dataset_info_filename = 'DATASET_INFO.tsv';  % Set the name of the dataset info file 
+diagnostic_folder_name = '_test';  % Set the name for the folders with diagnostic tests
+
 single_file = false; % preprocess a single file
 
 %% Select Preferences
@@ -23,17 +26,14 @@ single_file = false; % preprocess a single file
 dataset_name = [];  
 
 % Set the name and the path of the file
-%raw_filename = ['sub-hc10_ses-hc_task-rest_eeg.bdf']; 
-%raw_filepath = ['E:\02_Documenti\05_PhD\1°_anno\EEG_Prep\Datasets\ds002778\sub-hc10\ses-hc\eeg\'];
-raw_filename = [];
-raw_filepath = [];
+raw_filename = []; %raw_filename = ['sub-hc10_ses-hc_task-rest_eeg.bdf']; 
+raw_filepath = []; %raw_filepath = ['E:\02_Documenti\05_PhD\1°_anno\EEG_Prep\Datasets\ds002778\sub-hc10\ses-hc\eeg\'];
 
-% Set the name of the dataset info file
-dataset_info_filename = 'DATASET_INFO.tsv';  
 
 % Set how many files to preprocess (insert a number or 'all')             
 numbers_files = struct('N_subj',1,'N_sess',1,'N_obj',1);  
 
+%% Select parameters 
 % Create a struct to store the save information                            
 save_info = struct('save_data',true, ...
                    'save_data_as','matrix', ...
@@ -150,10 +150,7 @@ end
 csv_preprocessed_folder   = [root_data_path '_csv_marker_files/'];     
 if ~exist(csv_preprocessed_folder, 'dir') && save_info.save_marker 
     mkdir(csv_preprocessed_folder)
-end
-
-% Set the name for the folders with diagnostic tests
-diagnostic_folder_name = '_test';     
+end   
 
 %% Lunch Preprocess of the Datasets
 % Create two use modes: if dataset name is specified, preprocess only that
