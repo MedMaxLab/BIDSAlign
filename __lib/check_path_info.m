1126bbb27b8fc9cb342ce221e739fbcaf52c127c
fedepup <federico.delpup@studenti.unipd.it>
2024-02-01 16:56:39 +0100

    solved bugs in path setting

--- __lib/check_path_info.m
+++ __lib/check_path_info.m
@@ -23,7 +23,7 @@ function [] = check_path_info(path_info, must_give_datasets_path)
     validPath = @(x) isfolder(x);
     
     if ~isempty(path_info.datasets_path)
-        if ~validPath(path_info.datasets_path) && ~ischar(path_info.datasets_path)
+        if ~(validPath(path_info.datasets_path) && ischar(path_info.datasets_path))
             error(' datasets path is not valid')
         end
     else
@@ -33,37 +33,37 @@ function [] = check_path_info(path_info, must_give_datasets_path)
     end
 
     if ~isempty(path_info.output_path)
-        if ~validPath(path_info.output_path) && ~ischar(path_info.output_path)
+        if ~(validPath(path_info.output_path) && ischar(path_info.output_path))
             error(' output path is not valid')
         end
     end
     
     if ~isempty(path_info.output_mat_path)
-        if ~validPath(path_info.output_mat_path) && ~ischar(path_info.output_mat_path)
+        if ~(validPath(path_info.output_mat_path) && ischar(path_info.output_mat_path))
             error(' output mat path is not valid')
         end
     end
     
     if ~isempty(path_info.output_csv_path)
-        if ~validPath(path_info.output_csv_path) && ~ischar(path_info.output_csv_path)
+        if ~(validPath(path_info.output_csv_path) && ischar(path_info.output_csv_path))
             error(' output csv path is not valid')
         end
     end
     
     if ~isempty(path_info.output_set_path)
-        if ~validPath(path_info.output_set_path) && ~ischar(path_info.output_set_path)
+        if ~(validPath(path_info.output_set_path) && ischar(path_info.output_set_path))
             error(' output set path is not valid')
         end
     end
     
     if ~isempty(path_info.eeglab_path)
-        if ~validPath(path_info.eeglab_path) && ~ischar(path_info.eeglab_path)
+        if ~(validPath(path_info.eeglab_path) && ischar(path_info.eeglab_path))
             error(' eeglab path is not valid')
         end
     end
     
     if ~isempty(path_info.raw_filepath)
-        if ~validPath(path_info.raw_filepath) && ~ischar(path_info.raw_filepath)
+        if ~(validPath(path_info.raw_filepath) && ischar(path_info.raw_filepath))
             error(' path to eeg single file (raw_filepath) is not valid')
         end
     end
--- __lib/search_eeglab_path.m
+++ __lib/search_eeglab_path.m
@@ -1,11 +1,13 @@
 
-function [] = search_eeglab_path(verbose)
+function [] = search_eeglab_path(verbose, forcestart, nogui_launch)
     % FUNCTION: search_eeglab_path
     %
     % Description: Searches for the path to the EEGLAB toolbox in your current search path list and adds it to the MATLAB search path.
     % In case it is not found, this function will try to search and add automatically the path 
-    % by looking at the toolbox folder and in your current working directory. 
-    % If nothing is found, a waring is generated and the user is asked to add the path
+    % by looking at the toolbox folder and in your current working
+    % directory. Note that by deafult the function will look if the eeglab.m file
+    % exist in your current search path.
+    % If nothing is found, a warning is generated and the user is asked to add the path
     % manually by running the following command:
     % 
     %     addpath(' path/to/eeglab')
@@ -14,22 +16,49 @@ function [] = search_eeglab_path(verbose)
     %   search_eeglab_path(verbose)
     %
     % Input:
-    %   - verbose (optional): A boolean flag indicating whether to display additional information (default: false).
+    %   - verbose (optional): A boolean flag indicating whether to display 
+    %           additional information (default: false).
+    %   - forcestart (optional): A boolean indicating whether to try to
+    %           actually run the command "eeglab" or not. (default: true)
+    %   - nogui_launch (optional): A boolean indicating whether to try to
+    %           launch eeglab with the nogui option or not. (default: true)
     %
     % Author: [Federico Del Pup]
     % Date: [25/01/2024]
     %
 
-    if nargin ~= 1
-        verbose = false;
+    switch nargin
+        case 0
+            verbose = false;
+            forcestart = false;
+            nogui_launch = true;
+        case 1
+            forcestart = false;
+            nogui_launch = true;
+        case 2
+            nogui_launch = true;
     end
     
-    try 
-        [~] = evalc('eeglab nogui;');
-        return
-    catch
-        if verbose
-            disp("eeglab not present in your current search path. Trying to add it automatically")
+    if forcestart
+        try 
+            if nogui_launch
+                [~] = evalc('eeglab nogui;');
+            else
+                [~] = evalc('eeglab; close;');
+            end
+            return
+        catch
+            if verbose
+                disp("eeglab not present in your current search path. Trying to add it automatically")
+            end
+        end
+    else 
+        if isequal(exist('eeglab','file'),2)
+            return
+        else
+            if verbose
+                disp("eeglab not present in your current search path. Trying to add it automatically")
+            end
         end
     end
 
@@ -72,15 +101,31 @@ function [] = search_eeglab_path(verbose)
             end
             addpath(folder_names{i})
             addedpath = true;
-            try 
-                [~] = evalc('eeglab nogui;');
-                if verbose
-                    disp('Found path to EEGLab')
+            if forcestart
+                try 
+                    if nogui_launch
+                        [~] = evalc('eeglab nogui;');
+                    else
+                        [~] = evalc('eeglab; close;');
+                    end
+                    if verbose
+                        disp('Found path to EEGLab')
+                    end
+                    return
+                catch
+                    totPath = totPath + 1;
+                    pathAddedList{totPath} = folder_names{i};
                 end
-                return
-            catch
-                totPath = totPath + 1;
-                pathAddedList{totPath} = folder_names{i};
+            else
+                if isequal(exist('eeglab','file'),2)
+                    if verbose
+                        disp('Found path to EEGLab')
+                    end
+                    return
+                else
+                    totPath = totPath + 1;
+                    pathAddedList{totPath} = folder_names{i};
+                end
             end
         end
     end
@@ -101,15 +146,31 @@ function [] = search_eeglab_path(verbose)
             end
             addpath(folder_names{i})
             addedpath = true;
-            try 
-                [~] = evalc('eeglab nogui;');
-                if verbose
-                    disp('Found path to EEGLab')
+            if forcestart
+                try 
+                    if nogui_launch
+                        [~] = evalc('eeglab nogui;');
+                    else
+                        [~] = evalc('eeglab; close;');
+                    end
+                    if verbose
+                        disp('Found path to EEGLab')
+                    end
+                    return
+                catch
+                    totPath = totPath + 1;
+                    pathAddedList{totPath} = folder_names{i};
                 end
-                return
-            catch
-                totPath = totPath + 1;
-                pathAddedList{totPath} = folder_names{i};
+            else
+                if isequal(exist('eeglab','file'),2)
+                    if verbose
+                        disp('Found path to EEGLab')
+                    end
+                    return
+                else
+                    totPath = totPath + 1;
+                    pathAddedList{totPath} = folder_names{i};
+                end
             end
         end
     end
@@ -120,7 +181,12 @@ function [] = search_eeglab_path(verbose)
             disp(pathAddedList{1:totPath})
         end
         try 
-            [~] = evalc('eeglab nogui;'); % just to be sure that it does not run
+            % just to be sure that it does not run
+            if nogui_launch
+                [~] = evalc('eeglab nogui;');
+            else
+                [~] = evalc('eeglab; close;');
+            end
         catch
             error("added paths were not associated to the eeglab toolbox. " +...
                 "Please add the correct path manually by running the following " + ...
