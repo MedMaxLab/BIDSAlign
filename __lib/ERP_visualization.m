df5c76d26a397a608174e311448c23282fe3521e
andreazanola <andrea.zanola98@gmail.com>
2024-03-08 16:54:19 +0100

    add support to ERP visual

---
+++ __lib/ERP_visualization.m
@@ -0,0 +1,227 @@
+function ERP_visualization(folder,dataset,groups,pipelines,filename,save_img,event_name,epoch_lims,exclude_subj,verbose)
+
+    smooth = 5;
+    channels = ["FZ","PZ"];
+    colors = {'r','b','g','m','k','c','y'};
+    
+    
+    cmap = colormap('jet');
+    
+    folder = [folder '/' dataset];
+    group = [groups{1} '_' pipelines{1}];
+    a = dir([folder group '/*.set']);
+    out = size(a,1)-length(exclude_subj);        
+    path_file = [a(1).folder '/' a(1).name];
+    
+    [~,EEG] = evalc("pop_loadset(path_file);");
+    [~,EEG] = evalc("pop_epoch(EEG, event_name, epoch_lims, 'epochinfo', 'yes');");
+    [~,EEG] = evalc("pop_rmbase(EEG, [epoch_lims(1) 0] ,[]);");
+    [a,LERP,c] = size(EEG.data);
+    
+    listB = strings(1,length(EEG.chanlocs));
+    for t=1:length(EEG.chanlocs)
+        listB(t) = string(EEG.chanlocs(t).labels);                             
+    end
+    
+    if length(groups)>=1 && length(pipelines)==1
+        for i=1:length(groups)
+            group = [groups{i} '_' pipelines{1}];
+            [ERP] = get_group_ERP([folder group], filename, listB, listB, event_name, epoch_lims, LERP, exclude_subj, verbose);
+            evalc(['ERP_' pipelines{i} '=ERP']);
+        end
+        gint = groups;
+        pint = pipelines;
+    elseif length(pipelines)>1 && length(groups)==1
+        for i=1:length(pipelines)
+            group = [groups{1} '_' pipelines{i}];
+            [ERP] = get_group_ERP([folder group], filename, listB, listB, event_name, epoch_lims, LERP, exclude_subj, verbose);
+            evalc(['ERP_' pipelines{i} '=ERP']);
+        end
+        gint = groups;
+        pint = pipelines;
+        groups = pipelines;
+        pipelines = gint;
+    end
+    
+    FigH1 = figure('Position', get(0, 'Screensize'));
+    tiledlayout(length(channels),1, 'Padding', 'compact', 'TileSpacing', 'tight');
+    dt = 0.1;
+    for i=1:length(channels)
+        nexttile;
+        for j=1:length(groups)
+            ERP = eval(['ERP_' groups{j}]);
+            [d,~,~] = size(ERP);
+            norm_factor = 1/sqrt(d-1);
+            [channel_index, ~] = get_indexch(listB, channels(i));
+            m = squeeze(mean(ERP(:,:,channel_index),1));
+            s = squeeze(std(ERP(:,:,channel_index),[],1));
+            
+            shadedErrorBar(EEG.times, m, s*norm_factor,{'Color',colors{j}}); hold on;
+        end
+        title(['AVG ERP | ' convertStringsToChars(channels(i))]);
+        lgd = legend(groups);
+        lgd.Location ='southwest';
+        ax = gca;
+        ax.XTick = (epoch_lims(1):dt:epoch_lims(2))*1000;
+        xlabel('t [ms]');
+        ylabel('\muV');
+        xline(0,'k','HandleVisibility','off');
+        yline(0,'k','HandleVisibility','off');
+    end
+    
+    %% Plot ERP topography
+    times = [150 250 400 600];
+    
+    if verbose
+        verb = 'on';
+    else
+        verb = 'off';
+    end
+    
+    if length(EEG.chanlocs) > 20
+        electrode_mode = 'on';
+    else
+        electrode_mode = 'labels';
+    end
+    
+    FigH1 = figure('Position', get(0, 'Screensize'));
+    if length(groups)==2
+        tiledlayout(length(groups)+1,length(times), 'Padding', 'compact', 'TileSpacing', 'tight');
+    else
+        tiledlayout(length(groups),length(times), 'Padding', 'compact', 'TileSpacing', 'tight');
+    end
+    
+    for i=1:length(groups)
+        for j=1:length(times)
+            m = squeeze(mean(eval(['ERP_' groups{i}]),1));
+            [~,ind_t] = min(abs(EEG.times-times(j)));
+            nexttile;
+            topoplot(m(ind_t,:),EEG.chanlocs,'electrodes',electrode_mode,'maplimits','minmax','colormap',cmap,'verbose',verb);
+            title([num2str(EEG.times(ind_t)) ' ms | ' groups{i}]);
+    
+            cbar = colorbar;
+            cbar.Label.String = '\muV';
+            cbar.FontSize = 10;
+        end
+    end
+    
+    if length(groups)==2
+        for j=1:length(times)
+            m = squeeze(mean(eval(['ERP_' groups{1}])-eval(['ERP_' groups{2}]),1));
+            [~,ind_t] = min(abs(EEG.times-times(j)));
+            nexttile;
+            topoplot(m(ind_t,:),EEG.chanlocs,'electrodes',electrode_mode,'maplimits','minmax','colormap',cmap,'verbose',verb);
+            title([num2str(EEG.times(ind_t)) ' ms | Discrepancy ' groups{1} ' - ' groups{2}]);
+        
+            cbar = colorbar;
+            cbar.Label.String = '\muV';
+            cbar.FontSize = 10;
+        end
+    end
+    
+    if isempty(filename)
+        if length(gint)==1
+            sgtitle([dataset ' - Group: ' pipelines{1} ' | AVG ERP Comparison']);
+        else
+            sgtitle([dataset ' - Pipeline: ' pipelines{1} ' | AVG ERP Comparison']);
+        end
+    else
+        sgtitle([filename ' | ERP Comparison'],'Interpreter','none');
+    end
+    
+    cmap = colormap('jet');
+    minPSD = 10^20;
+    maxPSD = 10^(-20);
+    
+    if ~isempty(filename) && ~isempty(event_name)
+        FigH2 = figure('Position', get(0, 'Screensize'));
+        tiledlayout(length(pipelines)+1,length(channels), 'Padding', 'compact', 'TileSpacing', 'tight');
+        count=1;
+        for i=1:length(pipelines)
+            for j=1:length(channels)
+                [~,EEG] = evalc("pop_loadset([path dataset groups{1} '_' pipelines{i} '/' filename '.set']);");
+                [~,EEG] = evalc("pop_epoch(EEG, event_name, epoch_lims, 'epochinfo', 'yes');");
+                [~,EEG] = evalc("pop_rmbase(EEG, [epoch_lims(1) 0] ,[]);");
+    
+                listB = strings(1,length(EEG.chanlocs));
+                for t=1:length(EEG.chanlocs)
+                    listB(t) = string(EEG.chanlocs(t).labels);                             
+                end
+    
+                [channel_index, ~] = get_indexch(listB, channels(j));
+                if isempty(channel_index)
+                    error('CHANNEL NOT FOUND');
+                else
+                    titl = [EEG.chanlocs(channel_index).labels ' | ' pipelines{i}];
+                end
+    
+                nexttile;
+                %pop_erpimage(EEG,1, [channel_index],[[]],title,smooth,1,{},[],'' ,...
+                %     'yerplabel','\muV','erp','on','cbar','on','topo', {[channel_index] EEG.chanlocs EEG.chaninfo });
+                [a,b,c] = size(EEG.data);
+                ERP_matrix = squeeze(EEG.data(channel_index,:,:))';
+                ERP_matrix_mean = movmean(ERP_matrix,smooth);
+    
+                imagesc(EEG.times,1:1:c,ERP_matrix_mean);
+                ylabel('Trials');
+                xlabel('Time [ms]');
+                cmap = colormap('jet');
+                title(titl);
+                evalc(['ax' num2str(count)  ' = gca']);    
+                evalc(['EEG_' pipelines{i} '=EEG']);
+                minPSD = min([minPSD min(ERP_matrix_mean,[],"all")]);
+                maxPSD = max([maxPSD max(ERP_matrix_mean,[],"all")]);
+                count = count+1;
+            end
+        end
+    
+        for i=1:length(pipelines)*length(channels)
+            ax = eval(['ax' num2str(i)]);
+            ax.LineWidth = 1.1;
+            ax.XTick = linspace(epoch_lims(1),epoch_lims(2),5)*1000;
+            cbar = colorbar(ax);
+            set(cbar, 'ylim', [minPSD maxPSD]);
+        end
+    
+    
+        for j=1:length(channels)
+            nexttile;
+            [channel_index, ~] = get_indexch(listB, channels(j));
+            for i=1:length(pipelines)
+                EEG = eval(['EEG_' pipelines{i}]);
+                channel_ERP_mean = squeeze(mean(EEG.data(channel_index,:,:),3)');
+                evalc(['ERP_' pipelines{i} '= channel_ERP_mean']);
+                channel_ERP_std  = squeeze(std(EEG.data(channel_index,:,:),[],3)');
+                norm_factor = 1/sqrt(c-1);
+                shadedErrorBar(EEG.times,channel_ERP_mean, channel_ERP_std*norm_factor,{'Color',colors{i}}); hold on;
+            end
+            title(['AVG ERP | ' EEG.chanlocs(channel_index).labels])
+            lgd = legend(pipelines);
+            lgd.Location ='southwest';
+            ax = gca;
+            ax.LineWidth = 1.1;
+            ax.XTick = linspace(epoch_lims(1),epoch_lims(2),5)*1000;
+            xlabel('Time [ms]');
+            ylabel('Potential [\muV]');
+        end
+    end
+    
+    if isempty(filename)
+        if length(gint)==1
+            sgtitle([dataset ' - Group: ' pipelines{1} ' | ERP Comparison']);
+        else
+            sgtitle([dataset ' - Pipeline: ' pipelines{1} ' | ERP Comparison']);
+        end
+    else
+        sgtitle([filename ' | ERP Comparison'],'Interpreter','none');
+    end
+
+    if ~isempty(save_img)
+        saveas(FigH1,[save_img dataset '_' pipelines{1} '_AVGERP.png']);
+        if ~isempty(filename)
+            saveas(FigH2,[save_img dataset '_' pipelines{1} '_ERP.png']);
+        end
+    end
+
+
+end
---
+++ __lib/get_group_ERP.m
@@ -0,0 +1,65 @@
+
+function [ERP_group] = get_group_ERP(folder, filename, channels, listB, event_name, epoch_lims, LERP, exclude_subj, verbose)
+    % FUNCTION: get_group_ERP
+    %
+    % Description: Extracts event-related potentials (ERPs) for a group of subjects from EEG data.
+    %
+    % Syntax:
+    %   [ERP_group] = get_group_ERP(folder, filename, channels, listB, event_name, epoch_lims, LERP, exclude_subj, verbose)
+    %
+    % Input:
+    %   - folder (string): Path to the folder containing EEG data files.
+    %   - filename (string): Optional. Name of the EEG data file. If provided, only the file with this name will be loaded.
+    %   - channels (cell array of strings): List of channel names for which ERPs will be extracted.
+    %   - listB (cell array of strings): List of all channel names in the dataset.
+    %   - event_name (string): Name of the event marker.
+    %   - epoch_lims (vector): Epoch limits [start_time, end_time].
+    %   - LERP (integer): Length of the ERP (in data points).
+    %   - exclude_subj (cell array of strings): List of subjects to exclude from analysis.
+    %   - verbose (logical): Flag indicating whether to display progress messages.
+    %
+    % Output:
+    %   - ERP_group (3D array): Extracted average ERP for each subject and channel desired.
+    %
+    % Author: [Andrea Zanola]
+    % Date: [08/03/2024]
+
+    if isempty(filename)
+        a = dir([folder '/*.set']);
+    else
+        a = dir([folder '/' filename '.set']);
+    end
+    out = size(a,1);
+
+    ERP_group = zeros(out-length(exclude_subj),LERP,length(channels)); 
+
+    t = 1;
+    for i=1:length(a)
+        path_file = [folder '/' a(i).name];
+    
+        c = true;
+        for k = 1:length(exclude_subj)
+            c = c && ~contains(path_file,exclude_subj{k});
+        end
+    
+        if c
+            if verbose
+                EEG = pop_loadset(path_file);
+                EEG = pop_epoch(EEG, event_name, epoch_lims, 'epochinfo', 'yes');
+                EEG = pop_rmbase(EEG, [epoch_lims(1) 0] ,[]);
+            else
+                [~,EEG] = evalc("pop_loadset(path_file);");
+                [~,EEG] = evalc("pop_epoch(EEG, event_name, epoch_lims, 'epochinfo', 'yes');");
+                [~,EEG] = evalc("pop_rmbase(EEG, [epoch_lims(1) 0] ,[]);");  
+            end
+        
+            for j=1:length(channels)
+                [channel_index, ~] = get_indexch(listB, channels(j));
+    
+                ERP_group(t,:,j) = squeeze(mean(EEG.data(channel_index,:,:),3));
+            end
+        end
+        t = t+1;
+    end
+
+end
